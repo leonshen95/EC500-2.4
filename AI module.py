d114659611620e4fdc5c796bd6bf286c6f177a91
YanzuWuu <43078590+YanzuWuu@users.noreply.github.com>
2019-02-07 19:41:11 -0500

    correct one naming mistakes

--- AI module.py
+++ AI module.py
@@ -1,6 +1,10 @@
 
 import numpy as np
 import DataBaseModule
+
+# Input: ID(from main function perhaps), infoDB(from Database function)
+# output: Three predicted parameters, three Alert signals(Type:Boolean
+
 class AI_module(object):
     def __init__(self, dict):
         self._dict = dict
@@ -20,7 +24,7 @@ class AI_module(object):
                 oxygen = dict.get('blood_oxygen')
                 Pulse = dict.get('blood_pulses')
                 Blood_pressure.append(pressure)
-                Blood_oxygen.append(Blood_oxygen)
+                Blood_oxygen.append(oxygen)
                 Pulses.append(Pulse)
 
 
@@ -43,19 +47,20 @@ class AI_module(object):
     def Feedback(self,Blood_pressure_predict_result, Blood_oxygen_predict_result, Pulse_predict_result):
         lower_BP= 80
         upper_BP= 120
-        BP_Alert= 0
         lower_BO = 80
         upper_BO = 120
-        BO_Alert = 0
         lower_Pulse = 80
         upper_Pulse = 120
-        Pulse_Alert =0
+        BP_Alert= False
+        BO_Alert = False
+
+        Pulse_Alert =False
         if(Blood_oxygen_predict_result<lower_BO or Blood_oxygen_predict_result>upper_BO):
-            BO_Alert =1
+            BO_Alert =True
         if(Blood_pressure_predict_result<lower_BP or Blood_pressure_predict_result>upper_BP):
-            BP_Alert =1
+            BP_Alert =True
         if(Pulse_predict_result< lower_Pulse or Pulse_predict_result >upper_Pulse):
-            Pulse_Alert =1
+            Pulse_Alert =True
         ## feedback the AI prediction restult to the interface
         ## It will turn on the Alert when the statues get worse.
         return BO_Alert,BP_Alert,Pulse_Alert

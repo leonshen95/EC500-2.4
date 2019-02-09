4ebc206850a61b5d9dcc358285b2f1b87c6a9417
JasonZ95 <43214200+JasonZ95@users.noreply.github.com>
2019-02-09 12:45:53 -0500

    Update input_api.py

--- input_api.py
+++ input_api.py
@@ -1,34 +1,47 @@
-class intput
-    def __init__(self, user_id, age, gender, heartrate, Systolic_BP, DIastolic_BP, blood_oxygen, temperature):
-        # self.user_id = user_id
-        # self.age = age
-        # self.gender = gender
-        # self.heartbeat = heartbeat
-        # self.blood_pressure = blood_pressure
-        # self.blood_oxygen = blood_oxygen
-        # self.temperature = temperature
-        #Systolic_BP
-        #Diastolic_BP
-        self.dic = {"user_id": user_id， "gender": gender, "heartrate": heartrate, "Diastolic_BP": Diastolic_BP, "Systolic_BP":Systolic_BP, "blood_oxygen": blood_oxygen, "temperature": temperature, "time": time}
+class input_api:
 
-    def filter(data, noise, data_type):
-        wrong = -100
+    def __init__(self, user_id, age, gender, heartrate, Systolic_BP, Diastolic_BP, blood_oxygen, temperature, time):
+
+        self.user_id = user_id
+        self.age = age
+        self.gender = gender
+        self.heartrate = heartrate
+        self.Systolic_BP = Systolic_BP
+        self.Diastolic_BP = Diastolic_BP
+        self.blood_oxygen = blood_oxygen
+        self.temperature = temperature
+        self.time = time
+        self.dic = {"user_id": user_id, "gender": gender, "heartrate": heartrate,
+                    "Diastolic_BP": Diastolic_BP, "Systolic_BP":Systolic_BP, "blood_oxygen": blood_oxygen, 
+                    "temperature": temperature, "time": time}
+
+    def filter(data):
+        wrong_flag = -1
+        noise = 500
         if data > noise:
-            data = wrong
+            data = wrong_flag
         return data
 
-    def return data(dic):
-        for key in dic.keys():
-            if (key != "user_id" and key != "age" and key != "gender"):
-                dic[key] = filter(dic[key], noise, key)
+
+
+    def implement_filter(self):
+        for key in self.dic.keys():
+            if (key != "user_id" and key != "age" and key != "gender" and key != "time"):
+                tmp = filter(self.dic[key])
+                self.dic[key] = tmp
+
+
 
     def return_request(wire):
         alert = 1
         data_db = 2
         if (wire == alert):
-            return user_data
+            user_data_dic = {"heartrate": heartrate,
+                    "Diastolic_BP": Diastolic_BP, "Systolic_BP":Systolic_BP, "blood_oxygen": blood_oxygen, 
+                    "temperature": temperature, "time": time}
+            return user_data_dic
         if (wire == data_db):
-            return user_data & user_info
+            return dic
 
 
 

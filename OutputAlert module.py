8c98d94f3ec33a063854ce8aa1e38d0284e27a64
Joey <43118582+JesusJoey@users.noreply.github.com>
2019-02-09 16:47:57 -0500

    Update OutputAlert module.py

--- OutputAlert module.py
+++ OutputAlert module.py
@@ -1,49 +1,47 @@
+import numpy as np
+from AI_module import AI_module
 def receive_basic_iuput_data(Singal_Loss, Shock_Alert, Oxygen_Supply, Fever, Hypotension, Hypertension):
  ##Recevie data from input module, then analyze it using some judge functions to generate boolean result
  ##Boolean Parameters
  ##If paramter returns True, means it should be alerted, then add it to the array
-    BasicResult = []
+    BasicResult = {'Signal_Loss':False, 'Shock_Alert':False,'Oxygen_Supply':False,'Fever':False,'Hypotension':False,'Hypertension':False}
     if(Singal_Loss == True):
-        BasicResult.append(Singal_Loss)
+        BasicResult['Signal Loss']=True
     if(Shock_Alert == True):
-        BasicResult.append(Shock_Alert) 
+        BasicResult['Shock_Alert']=True 
     if(Oxygen_Supply == True):
-        BasicResult.append(Oxygen_Supply)
+        BasicResult['Oxygen_Supply']=True 
     if(Fever == True):
-        return BasicResult.append(Fever) 
+        BasicResult['Fever']=True
     if(Hypotension == True):
-        BasicResult.append(Hypotension)
+        BasicResult['Hypotension']=True
     if(Hypertension == True):
-        BasicResult.append(Hypertension) 
+        BasicResult['Hypertension']=True 
 
     return BasicResult
 
 
-def send_basic_input_data(BasicResult, BasicData):
- ## Receive the result and show it on terminal or web page
-    sentData = analyze(BasicResult)
-    return sentData, BasicData
 
-def receive_AI_iuput_data(Singal_Loss, Shock_Alert, Oxygen_Supply, Fever, Hypotension, Hypertension):
+#def send_basic_input_data(BasicResult, BasicData):
+ ## Receive the result and show it on terminal or web page
+ #   sentData = analyze(BasicResult)
+ #   return sentData, BasicData
+
+
+def display_AI_iuput_data():
  ## Recevie AI data from input module, then analyze it using some judge functions to generate boolean result
  ## Paramter is boolean
  ## If paramter is True, means it should be alerted, then add it to the array
-    AIResult = []
-    if(Singal_Loss == True):
-        AIResult.append(Singal_Loss) 
-    if(Shock_Alert == True):
-        AIResult.append(Shock_Alert) 
-    if(Oxygen_Supply == True):
-        AIResult.append(Oxygen_Supply)
-    if(Fever == True):
-        return AIResult.append(Fever) 
-    if(Hypotension == True):
-        AIResult.append(Hypotension)
-    if(Hypertension == True):
-        AIResult.append(Hypertension) 
-    return AIresult
-
-def send_AI_input_data(AIResult):
+  
+    AI_module.AI_Module(Blood_oxygen, Blood_pressure, Pulses)
+    print('blood pressure prediction:')
+    print(pressure_predict_result)
+    print('blood oxygen prediction:')
+    print(oxygen_predict_result)
+    print('Pulse_predict_result:')
+    print(Pulse_predict_result)
+    
+#def send_AI_input_data(AIResult):
  ## Receive the result and show it on terminal or web page
-    sentData = analyze(AIResult)
-    return sentData
+ #   sentData = analyze(AIResult)
+ #   return sentData

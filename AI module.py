5cc11058ef0f701f0fd3f511139073e9a68d1f4e
YanzuWuu <43078590+YanzuWuu@users.noreply.github.com>
2019-02-10 14:30:17 -0500

    Update AI module.py
    Make the parameters' names the same with the ones in other modules

--- AI module.py
+++ AI module.py
@@ -11,46 +11,48 @@ class AI_module(object):
     def Query_Data_From_Database(self,ID,infoDB):
         ## connect database, query previous one day data from Database
         # Database = database_dict()
-        Blood_oxygen = []
-        Blood_pressure = []
-        Pulses = []
-        
-        info = DataBaseModule().search(ID)
+        Blood_oxygen=list()
+        Blood_pressure = list()
+        Systolic= list()
+        Diastolic= list()
+        DataBaseModule.search(ID)
         # Username = input("")
         #get dictionary from database
         for key in infoDB:
             if key== ID:
-                pressure = info['blood_pressure']
-                oxygen = info['blood_oxygen']
-                Pulse = info['blood_pulses']
+                pressure = dict.get('blood_pressure')
+                oxygen = dict.get('blood_oxygen')
+                Diastolic_BP = dict.get('Diastolic_BP')
+                Systolic_BP = dict.get('Systolic_BP')
                 Blood_pressure.append(pressure)
                 Blood_oxygen.append(oxygen)
-                Pulses.append(Pulse)
+                Systolic.append(Systolic_BP)
+                Diastolic.append(Diastolic_BP)
+
+        return Blood_oxygen, Blood_pressure, Systolic,Diastolic
 
 
-        return Blood_oxygen, Blood_pressure, Pulses
+    def AI_Module(self,Blood_oxygen, Blood_pressure, Systolic,Diastolic):
 
-
-    def AI_Module(self,Blood_oxygen, Blood_pressure, Pulses):
-
-        ## AI module do the prediection, The AI module uses previous data
+        ## AI module do the prediction, The AI module uses previous data
         oxygen=np.array(Blood_oxygen)
         pressure = np.array(Blood_pressure)
-        Pulse = np.array(Pulses)
+        diastolic = np.array(Diastolic)
+        systolic = np.array(Systolic)
         pressure_predict_result = np.mean(pressure)
         oxygen_predict_result=np.mean(oxygen)
-        Pulse_predict_result = np.mean(Pulse)
+        Diastolic_predict_result = np.mean(diastolic)
+        Systolic_predict_result = np.mean(systolic)
 
-        return pressure_predict_result, oxygen_predict_result, Pulse_predict_result
+
+        return pressure_predict_result, oxygen_predict_result, Diastolic_predict_result, Systolic_predict_result
 
 
-    def Feedback(self,Blood_pressure_predict_result, Blood_oxygen_predict_result, Pulse_predict_result):
+    def Feedback(self,Blood_pressure_predict_result, Blood_oxygen_predict_result, Diastolic_predict_result, Systolic_predict_result):
         lower_BP= 80
         upper_BP= 120
         lower_BO = 80
         upper_BO = 120
-        lower_Pulse = 80
-        upper_Pulse = 120
         BP_Alert= False
         BO_Alert = False
 
@@ -59,9 +61,9 @@ class AI_module(object):
             BO_Alert =True
         if(Blood_pressure_predict_result<lower_BP or Blood_pressure_predict_result>upper_BP):
             BP_Alert =True
-        if(Pulse_predict_result< lower_Pulse or Pulse_predict_result >upper_Pulse):
+        if(Systolic_predict_result< 90 or Diastolic_predict_result <60 or Systolic_predict_result > 140 or Diastolic_predict_result > 90):
             Pulse_Alert =True
-        ## feedback the AI prediction restult to the interface
+        ## feedback the AI prediction result to the interface
         ## It will turn on the Alert when the statues get worse.
         return BO_Alert,BP_Alert,Pulse_Alert
 
